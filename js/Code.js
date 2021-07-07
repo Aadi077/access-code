96b0af4da193dcfc0b2a974c353b4b0e06f0a3c4
Aadi077 <78770737+Aadi077@users.noreply.github.com>
2021-07-07 10:48:22 +0800

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,30 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+  
+    fill("white")
+    textSize(15)
+    text("N F U T O C I N", 700,150)
+    fill("lightblue")
+    text("Hint: Completes a task for you.", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("E B D T A A A S", 100,250)
+    fill("lightblue")
+    text("Hint: Stores your information.", 100,270)
+
+
+
+    // add code for displaying rest of the names and the hints.
+    
+
+}
